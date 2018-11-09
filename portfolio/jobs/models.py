3ff7383d37e217c7970542ef0e30734820021e82
Shiva Gangula <ashiva44@gmail.com>
2018-11-09 16:06:38 +0530

    job home page ready

--- portfolio/jobs/models.py
+++ portfolio/jobs/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
+
 # Create your models here.
 class job(models.Model):
     imageField = models.ImageField(upload_to='images/')
-    summary = models.CharField(max_length =200)
+    summary = models.CharField(max_length=200)
